67635727e397fba127f19661371011b1e4fb4015
AnnaGlusko666 <annaglusko225@gmail.com>
2024-06-11 15:35:13 +0300

    Update Tik-tak.cpp

--- Tik-tak/Tik-tak.cpp
+++ Tik-tak/Tik-tak.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
-#include <cstring> 
+#include <cstring>
 
 char board[3][3] = { {'1','2','3'}, {'4','5','6'}, {'7','8','9'} };
 char current_marker;
 int current_player;
-int move_count = 0; 
-int player1_wins = 0; 
-int player2_wins = 0; 
+int move_count = 0;
+int player1_wins = 0;
+int player2_wins = 0;
 std::ofstream logFile;
 
 void log(const std::string& message) {
     std::time_t now = std::time(nullptr);
     char* dt = std::ctime(&now);
-    dt[std::strlen(dt) - 1] = '\0'; 
+    dt[std::strlen(dt) - 1] = '\0';
     logFile << "[" << dt << "] " << message << "\n";
 }
 
@@ -40,16 +40,13 @@ bool placeMarker(int slot) {
 
 int winner() {
     for (int i = 0; i < 3; i++) {
-        
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
             return current_player;
         }
-        
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
             return current_player;
         }
     }
-    
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
         return current_player;
     }
@@ -65,7 +62,6 @@ void swapPlayerAndMarker() {
     } else {
         current_marker = 'X';
     }
-
     if (current_player == 1) {
         current_player = 2;
     } else {
@@ -84,7 +80,7 @@ void saveGame() {
     }
     boardFile.close();
 
-    playerFile << current_player << current_marker;
+    playerFile << current_player << ' ' << current_marker;
     playerFile.close();
 
     std::cout << "Гра збережена!\n";
@@ -117,7 +113,7 @@ void loadGame() {
 void resetBoard() {
     char initial_board[3][3] = { {'1','2','3'}, {'4','5','6'}, {'7','8','9'} };
     std::memcpy(board, initial_board, 9 * sizeof(char));
-    move_count = 0; // Скидання кількості ходів
+    move_count = 0;
 }
 
 void game() {
@@ -129,10 +125,19 @@ void game() {
 
     if (loadChoice == 'y') {
         loadGame();
+        if (winner() != 0) {
+            std::cout << "Завантажена гра вже завершена. Розпочніть нову гру.\n";
+            resetBoard();
+        }
     } else {
         std::cout << "Гравець 1, оберіть ваш маркер (X або O): ";
         char marker_p1;
         std::cin >> marker_p1;
+
+        while (marker_p1 != 'X' && marker_p1 != 'O') {
+            std::cout << "Невірний маркер! Будь ласка, оберіть X або O: ";
+            std::cin >> marker_p1;
+        }
 
         current_player = 1;
         current_marker = marker_p1;
@@ -153,11 +158,11 @@ void game() {
 
             if (slot == 0) {
                 saveGame();
-                break;
+                return; // Завершение игры после сохранения
             }
 
             if (slot < 1 || slot > 9) {
-                std::cout << " Невірний слот! Спробуйте ще раз.\n";
+                std::cout << "Невірний слот! Спробуйте ще раз.\n";
                 log("Гравець " + std::to_string(current_player) + " обрав невірний слот " + std::to_string(slot));
                 i--;
                 continue;
@@ -169,20 +174,20 @@ void game() {
                 continue;
             }
 
-            move_count++; // Збільшуємо кількість ходів
+            move_count++;
             log("Гравець " + std::to_string(current_player) + " поставив маркер в слот " + std::to_string(slot));
             drawBoard();
             player_won = winner();
 
             if (player_won == 1) {
                 player1_wins++;
-                std::cout << "Гравець 1 переміг за " << move_count << " ходів!\n"; // Виводимо кількість ходів
+                std::cout << "Гравець 1 переміг за " << move_count << " ходів!\n";
                 log("Гравець 1 переміг!");
                 break;
             }
             if (player_won == 2) {
                 player2_wins++;
-                std::cout << "Гравець 2 переміг за " << move_count << " ходів!\n"; // Виводимо кількість ходів
+                std::cout << "Гравець 2 переміг за " << move_count << " ходів!\n";
                 log("Гравець 2 переміг!");
                 break;
             }
